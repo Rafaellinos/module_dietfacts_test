575a4fc0280fa24c76292ac1886ef731634624fb
Rafaellinos <rafael.veloso.lino@hotmail.com>
2019-06-23 08:44:17 -0700

    adding openerpg file

---
+++ __openerp__.py
@@ -0,0 +1,7 @@
+{
+    'name' : "DietFacts",
+    'version' : "1.0",
+    'author' : "Rafael Lino, https://rafaellinos.github.io/rafaelino.github.io/", 
+    'depends' : ['sale'],
+    'installable' : True,
+}
