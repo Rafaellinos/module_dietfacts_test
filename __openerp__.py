97a9fcad210b9eb8f1be90edd46055325b923523
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-06-23 20:53:49 -0300

    Adding a description

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,7 @@
 {
     'name' : "DietFacts",
     'version' : "1.0",
+    'description' : 'Adds nutricition infromation to produtcs',
     'author' : "Rafael Lino, https://rafaellinos.github.io/rafaelino.github.io/", 
     'depends' : ['sale'],
     'installable' : True,
