933dd7189ab6373e06d478b7edebe9b0e12bd3b0
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-06-23 21:48:10 -0300

    Create field calories on model product.template

--- dietfacts.py
+++ dietfacts.py
@@ -1,1 +1,10 @@
 # Dietfacts application
+from openerp import models, fields
+
+# Extebd product.template model with calories
+
+class Dietfacts_product_template(models.Model):
+    _name = 'product.template'
+    _inherit = 'product.template'
+
+    calories = fields.Integer(string=u'Calories')
