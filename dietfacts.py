e3495843e4dafec1cc56eeceab942b0945332e27
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-06-29 10:19:16 -0300

    adding dietfacts res.users meal

--- dietfacts.py
+++ dietfacts.py
@@ -11,3 +11,16 @@ class Dietfacts_product_template(models.Model):
     serving_size = fields.Float(string=u'Serving Size')
     last_updated = fields.Date(string=u'Last Update')
     is_diet_item = fields.Boolean(string=u'Diet Item')
+
+class Dietfacts_res_users_meal(models.Model):
+    _name = 'res.users.meal'
+
+    name = fields.Char(string=u'Meal Name')
+    meal_date = fields.Datetime(string=u'Menu Date')
+    #item_ids = fields.One2many(
+    #    comodel_name='',
+    #    string=u'Items List')
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Meal Users')
+    notes = fields.Text(string=u'Meal Notes')
