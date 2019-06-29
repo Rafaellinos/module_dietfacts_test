2906e06ea021958afed764f5098bc24877cad152
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-06-29 10:35:31 -0300

    fixing model

--- dietfacts.py
+++ dietfacts.py
@@ -1,7 +1,7 @@
 # Dietfacts application
 from openerp import models, fields
 
-# Extebd product.template model with calories
+# Extend product.template model with calories
 
 class Dietfacts_product_template(models.Model):
     _name = 'product.template'
