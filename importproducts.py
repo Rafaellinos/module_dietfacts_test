683d30caed1bfd4ce1fb40ec4acffea9aa2e7899
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-07-04 19:52:27 -0300

    adding teste for authentication

--- importproducts.py
+++ importproducts.py
@@ -9,6 +9,12 @@ pwd = "admin"
 
 common = xmlrpclib.ServerProxy( '%s/xmlrpc/2/common' % server)
 
-print (common.version())
+#print (common.version())
+#Run this code below(with print) to see if it is working. Should return
+#some server and version information
+
+uid = common.authenticate(database, user, pwd, {})
+
+print (uid)
 
 
